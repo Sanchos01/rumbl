763238f4f90147eacf3b36653ce9e0392da1ac39
Alexander Tuktarov <sanchos.1991@mail.ru>
2017-05-17 20:52:49 +0300

    adding default user, wip

--- apps/rumbl/priv/repo/seeds.exs
+++ apps/rumbl/priv/repo/seeds.exs
@@ -11,8 +11,23 @@
 # and so on) as they will fail if something goes wrong.
 alias Rumbl.Repo
 alias Rumbl.Category
+alias Rumbl.User
+alias Rumbl.Video
+# alias Rumbl.Annotation
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
   Repo.get_by(Category, name: category) ||
     Repo.insert!(%Category{name: category})
 end
+
+# Adding default user with some video and annotations
+if Repo.get_by(User, name: "foobarbaz") do
+  IO.puts("Warning: user already exists")
+  else
+    %{name: name, username: username, password_hash: hash} = User.registration_changeset(%User{},
+      %{name: "foobarbaz", username: "foobarbaz123", password: "123 super secret"}).changes
+    Repo.insert!(%User{name: name, username: username, password_hash: hash})
+    IO.puts("Success: user created")
+end
+# user_id = Repo.get_by(User, name: "foobarbaz").id
+# if Repo.get_by(Video, user_id: user_id, title: title)

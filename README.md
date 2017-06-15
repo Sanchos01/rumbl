4acbea28d620f18274586489956b3b473755681d
александр туктаров <sanchos.1991@mail.ru>
2017-06-16 00:06:53 +0300

    add travis-ci

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rumbrella
-[![Deps Status](https://beta.hexfaktor.org/badge/all/github/Sanchos01/rumbl.svg)](https://beta.hexfaktor.org/github/Sanchos01/rumbl)
+[![Build Status](https://secure.travis-ci.org/Sanchos01/rumbl.svg "Build Status")](http://travis-ci.org/Sanchos01/rumbl) [![Deps Status](https://beta.hexfaktor.org/badge/all/github/Sanchos01/rumbl.svg)](https://beta.hexfaktor.org/github/Sanchos01/rumbl)
 
 **TODO: Add description**
--- apps/info_sys/test/backends/wolfram_test.exs
+++ apps/info_sys/test/backends/wolfram_test.exs
@@ -6,14 +6,13 @@ defmodule InfoSys.Backends.WolframTest do
     ref = make_ref()
     {:ok, pid} = Wolfram.start_link("1 + 1", ref, self(), 1)
     Process.monitor(pid)
-    assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}
+    assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}, 5_000
     assert_receive {:DOWN, _ref, :process, ^pid, :normal}
   end
 
   test "no query results reports an empty list" do
     ref = make_ref()
     {:ok, _} = Wolfram.start_link("none", ref, self(), 1)
-
-    assert_receive {:results, ^ref, []}
+    assert_receive {:results, ^ref, []}, 5_000
   end
 end
